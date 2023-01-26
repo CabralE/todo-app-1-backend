b91712a84f45a64b01a2a5a1d3ff31b063f92ac3
ed <cabrale234@gmail.com>
2023-01-26 13:12:37 -0500

    updating cors

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ import chalk from "chalk";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+const cors = require("cors");
 const app = express();
+app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
